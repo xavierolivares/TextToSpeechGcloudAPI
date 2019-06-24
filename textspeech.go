d5590e27725735bfd0ec80a8c32e3124b4f31eb1
Xavier Olivares <xm.olivares@gmail.com>
2019-06-24 00:43:58 -0400

    completed final testing

--- textspeech.go
+++ textspeech.go
@@ -1,23 +1,4 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
-// Command livecaption pipes the stdin audio data to
-// Google Speech API and outputs the transcript.
-//
-// As an example, gst-launch can be used to capture the mic input:
-//
-//    $ gst-launch-1.0 -v pulsesrc ! audioconvert ! audioresample ! audio/x-raw,channels=1,rate=16000 ! filesink location=/dev/stdout | livecaption
 package main
 
 import _ "github.com/joho/godotenv/autoload"
